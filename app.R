981909b94d538093101e847efd1bd1741f8e7d96
Jose Fernandez <40781886+josedv82@users.noreply.github.com>
2021-01-12 10:18:25 -0500

    Add files via upload

---
+++ app.R
@@ -0,0 +1,276 @@
+
+#############
+
+#Jose Fernandez
+#Jan 2021
+
+
+#############
+
+library(shiny)
+library(shinyjs)
+library(sparkline)
+library(tidyverse)
+
+source("data.R")
+
+#UI
+ui <- fluidPage(
+   
+  #theming   
+  theme = shinythemes::shinytheme("cyborg"),
+  tags$style(HTML('table.dataTable tr.selected td, table.dataTable td.selected {background-color: black !important;}')),
+  tags$head(tags$style("#modal1 .modal-body {background-color: white; padding: 10px}
+                       #modal1 .modal-content  {-webkit-border-radius: 6px !important;-moz-border-radius: 6px !important;border-radius: 6px !important;}
+                       #modal1 .modal-dialog { width: 880px; display: inline-block; text-align: left; vertical-align: top;}
+                       #modal1 .modal-header {background-color: #404764; border-top-left-radius: 6px; border-top-right-radius: 6px}
+                       #modal1 .modal { text-align: center; padding-right:0px; padding-top: 24px;}
+                       #moda1 .close { font-size: 16px}")),
+  
+  
+  #dependencies
+  shinyjs::useShinyjs(),
+  htmlwidgets::getDependency('sparkline'),
+  
+  tags$br(),
+  
+  #sidebar
+  sidebarLayout(
+    fluid = T,
+    sidebarPanel(width = 2, style = "position:fixed; width:16%",
+                 
+                
+                actionButton("info", "Info", icon = icon("info"), style = "border-color: #16a085; color: #16a085"),
+                actionButton("code", "Code", icon = icon("github"), style = "border-color: #16a085; color: #16a085", onclick ="window.open('https://github.com/josedv82/EPV_dashboard/blob/main/README.md', '_blank')"),
+                
+                tags$br(),
+                tags$hr(),
+                tags$br(),
+                 
+                 #team filter
+                shinyWidgets::checkboxGroupButtons(
+                   inputId = "teamf",
+                   label = "Select Team(s)",
+                   choices = c("<img src='https://cdn.freebiesupply.com/images/thumbs/2x/brooklyn-nets-logo.png' width=60px></img>" = "Home", 
+                               "<img src='https://cdn.freebiesupply.com/images/thumbs/2x/miami-heat-logo.png' width=60px></img>"  = "Away"),
+                   checkIcon = list(yes = icon("ok", lib = "glyphicon")),
+                   justified = TRUE,
+                   selected = c("Home", "Away")
+                 ),
+                
+                tags$br(),
+                tags$br(),
+                
+                
+                #select view filter
+                shinyWidgets::prettyRadioButtons(
+                  inputId = "view1",
+                  label = "Select View", 
+                  choices = c("Play by Play", "EPV Added"),
+                  icon = icon("check"), 
+                  bigger = TRUE,
+                  status = "info",
+                  animation = "jelly",
+                  selected = "Play by Play"
+                ),
+                
+                
+                
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$br(),
+                tags$hr(),
+                #links
+                p("Designed by Jose Fernandez", style = "color: #717d7e"),
+                actionButton(inputId='twitter', label="", icon = icon("twitter", lib = "font-awesome"), 
+                             onclick ="window.open('https://twitter.com/jfernandez__', '_blank')"),
+                actionButton(inputId='github', label="", icon = icon("github", lib = "font-awesome"), 
+                             onclick ="window.open('https://github.com/josedv82', '_blank')"),
+                a(actionButton(inputId = "email", label = "", icon = icon("envelope", lib = "font-awesome")),
+                  href="mailto:jose.fernandezdv@gmail.com")
+    
+    ),
+    
+      # table output
+      mainPanel(width = 10,
+        #conditions
+        conditionalPanel(condition = "input.view1",
+         DT::dataTableOutput("table")
+        ),
+        
+        conditionalPanel(condition= "input.view1",
+                         reactable::reactableOutput("epva")
+        )
+      )
+        
+      )
+   )
+
+# Define server logic 
+server <- function(input, output, session) {
+  
+  #info modal
+  observeEvent(input$info, {
+    
+    showModal(tags$div(id="modal1", modalDialog(
+      inputId = 'Dialog1', 
+      title = HTML('<span style="color:white; font-size: 20px; font-weight:bold; font-family:sans-serif ">About this app<span>
+                   <button type = "button" class="close" data-dismiss="modal" ">
+                   <span style="color:white; ">x <span>
+                   </button> '),
+      tags$br(),
+      p("This is a conceptual dashboard design to visualize and interact with Expected Possession Value (EPV) in the NBA.
+              It intends to have some resemblance with common stock trading apps, highlighting the EPV trend and overall change from
+              the start to end of the possession.", style = "color: #212f3c"),
+      p("For simplicity, this app only shows one game (Heat @ Nets, Nov 1st, 2013).", style = "color: #212f3c"),
+      tags$br(),
+      tags$h4("What is EPV?", style = "color: black"),
+      p("In brief, EPV is a framework for using optical player tracking data to estimate, in real time, the expected number of points obtained by the end of a possession (Cervone et al, 2016).", style = "color: #212f3c"),
+      tags$br(),
+      img(src='https://grantland.com/wp-content/uploads/2014/02/epv-chart-features.png', width="60%", align = "middle"),
+      tags$br(),
+      HTML(paste(tags$span("For more information about EPV visit the original", style = "color: #212f3c"), tags$span(tags$a(href= "https://arxiv.org/pdf/1408.0777.pdf", "work")), 
+                 tags$span("by Cervone et al, as well as this", style = "color: #212f3c"),
+                 tags$span(tags$a(href= "https://grantland.com/the-triangle/behind-databall-a-discussion-on-the-methodology-of-expected-points-value/", "interview")),
+                 tags$span("by the authors.", style = "color: #212f3c"))),
+      tags$br(),
+      tags$br(),
+      tags$h4("The data", style = "color: black"),
+      HTML(paste(tags$span("The data used throughout this app was extracted from Dan Cervone's EPV demo repository", style = "color: #212f3c"),
+      tags$span(tags$a(href="https://github.com/dcervone/EPVDemo ", "here.")))),
+      tags$br(),
+      tags$br(),
+      easyClose = TRUE,
+      footer = NULL )))
+    
+  })
+  
+  #reactive dataset for pbp filtering
+  aa <- reactive({
+    
+    req(input$teamf)
+    
+    a %>% filter(Offense1 %in% input$teamf)
+    })
+                 
+  aaa <- reactive({ aa() %>%   
+      
+    mutate(Animation = sapply(1:nrow(aa()), function(i){
+      sprintf("<button id='inf%d' type='button' class='btn btn-default action-button shiny-bound-input'><i class= 'glyphicon glyphicon-plus-sign'/></button>", i)
+    })) %>%
+    
+    formattable::formattable(
+      
+      list(
+        `EPV Difference` = formattable::formatter("span", 
+                                                  style = x ~ formattable::style(display = "block", 
+                                                                                 padding = "0 4px", 
+                                                                                 `border-radius` = "4px",
+                                                                                 
+                                                                                 `background-color` = ifelse(x > 0, "springgreen", ifelse(x < 0, "tomato", "gray"))),
+                                                  x ~ ifelse(x > 0, paste("+", x, sep = ""), x))
+        
+        
+      )
+      
+    )
+  })
+  
+  
+  #EPV table output
+  output$table <- DT::renderDT({
+    
+    validate(
+      need(input$teamf != "", "Please select at least one team")
+    )
+    
+    formattable::as.datatable(aaa() %>% 
+          select(-Offense1) %>%
+          select(Period, 
+          `Game Clock` = game_clock, 
+          `Poss N` = Poss,
+          Offense,
+         `Play by Play` = Play_by_Play, 
+          Score, 
+          `EPV Trend` = EPV_Trend,
+          `EPV Difference`,
+          Animation), 
+         
+                                rownames = FALSE,
+                                escape = F,
+                                selection = "single",
+                                class = 'white-space: nowrap',
+                                #extensions = 'Responsive',
+                                options = list(dom = 'tp',
+                                               pageLength = 500,
+                                               bSort=F,
+                                               paging = F,
+                                               #scrollY = "800px",
+                                               #scrollX = F,
+                                               drawCallback = htmlwidgets::JS('function(){debugger;HTMLWidgets.staticRender();}'),
+                                               columnDefs = list(
+                                                            list(className = 'dt-center', targets = c(0,1,2,3,5,6,7,8)),
+                                                            list(width = '50px', targets = c(0:3))
+                                                           ), 
+                                                      
+                                                      paging = F)) %>%
+                                 
+                                DT::formatStyle(c(1:9), backgroundColor = 'black') %>%
+                                DT::formatStyle(c(8,9), color = 'black') %>%
+                                DT::formatStyle(c(1:dim(aaa())[2]), border = '2px solid #060806')
+                                 
+                                  })
+                                 
+                                  
+  
+  #show EPV gif in modal on click 
+  observe(
+  for(i in 1:nrow(aa())){
+  shinyjs::onclick(paste0("inf",i), {
+      
+      showModal(
+        modalDialog(
+          img(src = paste0("zplay_", aa()$Poss[input$table_rows_selected], ".gif", sep = ""), width="100%"),
+          footer = NULL,
+          size = "l",
+          easyClose = T
+          
+        )
+      )
+      
+    })
+    
+  }
+  )
+  
+  
+  #EPVA table
+  output$epva <- reactable::renderReactable({
+    EPVA_tab
+    
+  })
+  
+  
+  #dynamic showing/hiding based on filter selection
+  observeEvent(input$view1,{
+  if(input$view1 == "Play by Play"){
+    shinyjs::show("table")
+    shinyjs::hide("epva")
+  }else{
+  shinyjs::hide("table")
+  shinyjs::show("epva") 
+  }
+  })
+  
+  
+}
+  
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ data.R
@@ -0,0 +1,251 @@
+
+#############
+
+#Jose Fernandez
+#Jan 2021
+
+
+#############
+
+#EPV
+edat <- feather::read_feather("epv.feather")
+epva <- read.csv("epva.csv") %>%
+  mutate(Player = sprintf(paste0("<img src='", Photo, "' width=70px><div class='jhr'>%s</div></img>"), Player)) %>%
+  mutate(Team = ifelse(Team == "Away",  
+                          sprintf("<img src='https://cdn.freebiesupply.com/images/thumbs/2x/miami-heat-logo.png' width=90px><div class='jhr'>\r</div></img>", "Heat"), 
+                          sprintf("<img src='https://cdn.freebiesupply.com/images/thumbs/2x/brooklyn-nets-logo.png' width=90px><div class='jhr'>\r</div></img>", "Nets"))) %>%
+  select(Team, Player, EPVA)
+
+#PBP
+pbp <- read.csv("pbp.csv")
+
+#players
+players <- read.csv("players2013.csv")
+
+#prepare to replace player id with player name
+
+players <- players %>%
+  mutate(Player = paste(firstname, lastname, sep = " ")) %>%
+  select(player_id, Player)
+
+a1 <- players %>% select(PA1 = Player, a1_id = player_id)
+a2 <- players %>% select(PA2 = Player, a2_id = player_id)
+a3 <- players %>% select(PA3 = Player, a3_id = player_id)
+a4 <- players %>% select(PA4 = Player, a4_id = player_id)
+a5 <- players %>% select(PA5 = Player, a5_id = player_id)
+h1 <- players %>% select(PH1 = Player, h1_id = player_id)
+h2 <- players %>% select(PH2 = Player, h2_id = player_id)
+h3 <- players %>% select(PH3 = Player, h3_id = player_id)
+h4 <- players %>% select(PH4 = Player, h4_id = player_id)
+h5 <- players %>% select(PH5 = Player, h5_id = player_id)
+
+#join all player tables
+edat1 <- edat %>%
+  
+  full_join(a1, by = "a1_id") %>%
+  full_join(a2, by = "a2_id") %>%
+  full_join(a3, by = "a3_id") %>%
+  full_join(a4, by = "a4_id") %>%
+  full_join(a5, by = "a5_id") %>%
+  full_join(h1, by = "h1_id") %>%
+  full_join(h2, by = "h2_id") %>%
+  full_join(h3, by = "h3_id") %>%
+  full_join(h4, by = "h4_id") %>%
+  full_join(h5, by = "h5_id") %>%
+  
+  # select relevant metrics  
+  select(game, time, quarter, game_clock, shot_clock, 
+         ball_x, ball_y, ball_z,  
+         PA1, a1_x, a1_y,
+         PA2, a2_x, a2_y,
+         PA3, a3_x, a3_y,
+         PA4, a4_x, a4_y,
+         PA5, a5_x, a5_y,
+         PH1, h1_x, h1_y,
+         PH2, h2_x, h2_y,
+         PH3, h3_x, h3_y,
+         PH4, h4_x, h4_y,
+         PH5, h5_x, h5_y,
+         everything()) %>%
+  filter(possID != 0) %>%
+  
+  #translate events  
+  mutate_at(
+    vars(one_of('a1_event', 'a2_event', 'a3_event', 'a4_event', 'a5_event', 'h1_event', 'h2_event', 'h3_event', 'h4_event', 'h5_event')),
+    funs(case_when(
+      . == 1 ~ "Free Throw Made",
+      . == 2 ~ "Free Throw Missed",
+      . == 3 ~ "Shot Made",
+      . == 4 ~ "Shot Missed",
+      . == 5 ~ "Offensive Rebound",
+      . == 6 ~ "Defensive Rebound",
+      . == 7 ~ "Turnover",
+      . == 8 ~ "Foul",
+      . == 9 ~ "Violation",
+      . == 10 ~ "Substitution",
+      . == 11 ~ "Timeout",
+      . == 12 ~ "Jump Ball",
+      . == 13 ~ "Ejection",
+      . == 14 ~ "Start Period",
+      . == 15 ~ "End Period",
+      . == 16 ~ "Clock Sync",
+      . == 17 ~ "Instant Replay",
+      . == 18 ~ "Replay Ruling",
+      . == 19 ~ "Game Over",
+      . == 20 ~ "Stoppage",
+      . == 21 ~ "Dribble",
+      . == 22 ~ "Pass",
+      . == 23 ~ "",
+      . == 24 ~ "Shot Block",
+      . == 25 ~ "Assist",
+      TRUE ~ ""
+    ))) %>%
+  
+  #fill next x rows with events to help reading on gif
+  mutate_at(
+    vars(one_of('a1_event', 'a2_event', 'a3_event', 'a4_event', 'a5_event', 'h1_event', 'h2_event', 'h3_event', 'h4_event', 'h5_event')),
+    
+    funs(case_when(
+      
+      is.na(lag(.)) ~ .,
+      lag(.) != "" ~ lag(.),
+      lag(.,2) != "" ~ lag(.,2),
+      lag(.,3) != "" ~ lag(.,3),
+      lag(.,4) != "" ~ lag(.,4),
+      lag(.,5) != "" ~ lag(.,5),
+      lag(.,6) != "" ~ lag(.,6),
+      lag(.,7) != "" ~ lag(.,7),
+      lag(.,8) != "" ~ lag(.,8),
+      lag(.,9) != "" ~ lag(.,9),
+      lag(.,10) != "" ~ lag(.,10),
+      lag(.,11) != "" ~ lag(.,11),
+      lag(.,12) != "" ~ lag(.,12),
+      lag(.,13) != "" ~ lag(.,13),
+      lag(.,14) != "" ~ lag(.,14),
+      lag(.,15) != "" ~ lag(.,15),
+      lag(.,16) != "" ~ lag(.,16),
+      TRUE ~ .
+      
+    )))
+
+#prepare players table to identify which team is in offense
+players_1 <- players %>% select(poss = player_id, Player)
+
+edat1 <- edat1 %>%
+  full_join(players_1, by = "poss") %>%
+  group_by(possID) %>%
+  mutate(Possession = ifelse(Player == PA1, "Away",
+                             ifelse(Player == PA2, "Away",
+                                    ifelse(Player == PA3, "Away",
+                                           ifelse(Player == PA4, "Away",
+                                                  ifelse(Player == PA5, "Away", "Home")))))) %>%
+  fill(Possession, .direction = "downup") %>%
+  select(-Player)
+
+
+library(sparkline)
+library(DT)
+
+a <- edat1 %>%
+  select(Period = quarter, game_clock, possID, epv.smooth, Offense1 = Possession) %>%
+  filter(!is.na(epv.smooth)) %>%
+  mutate(epv.smooth = round(epv.smooth,3)) %>%
+  group_by(Period, Offense1, possID) %>%
+  mutate(game_clock = last(game_clock)) %>%
+  group_by(Period, game_clock, Offense1, possID) %>%
+  
+  summarise(
+    
+    `EPV Difference` = round(last(epv.smooth)-first(epv.smooth),3),
+    
+    EPV_Trend = spk_chr(epv.smooth, 
+                        type = "line", 
+                        height = 50,
+                        width = 200,
+                        lineColor = ifelse( `EPV Difference` > 0, "springgreen", "tomato"), 
+                        lineWidth = 1,
+                        fillColor = "transparent", 
+                        minSpotColor = "red",
+                        maxSpotColor = "green",
+                        spotColor = F,
+                        chartRangeClip = T,
+                        tooltipChartTitle = "EPV Trend")) %>% 
+  
+  full_join(pbp) %>%
+  
+  na.omit() %>%
+  
+  select(Period, game_clock, Poss = possID, Play_by_Play, Score, EPV_Trend, `EPV Difference`, Offense1) %>%
+  
+  mutate(Period = case_when(
+    Period == 1 ~ "1st",
+    Period == 2 ~ "2nd",
+    Period == 3 ~ "3rd",
+    Period == 4 ~ "4th",
+    TRUE ~ "NA"
+  )) %>% 
+  mutate(minutes = game_clock %/% 60) %>% 
+  mutate(seconds = round(game_clock %% 60,1)) %>%
+  mutate(game_clock = paste(minutes,"m ", seconds, "s", sep = "")) %>%
+  select(-minutes, -seconds) %>%
+  arrange(Period, Poss) %>%
+  mutate(Offense = ifelse(Offense1 == "Away",  
+                          sprintf("<img src='https://cdn.freebiesupply.com/images/thumbs/2x/miami-heat-logo.png' width=60px><div class='jhr'>\r</div></img>", "Heat"), 
+                          sprintf("<img src='https://cdn.freebiesupply.com/images/thumbs/2x/brooklyn-nets-logo.png' width=60px><div class='jhr'>\r</div></img>", "Nets"))) %>%
+  ungroup()
+
+
+
+
+
+library(htmltools)
+library(reactable)
+
+
+#function to create color grades for background
+orange_pal <- function(x) rgb(colorRamp(c("#cc3232", "#db7b2b", "#e7b416", "#99c140", "#2dc937"))(x), maxColorValue = 255)
+
+
+
+EPVA_tab <- reactable(epva,
+ 
+          pagination = FALSE,
+          highlight = TRUE,
+          showSortIcon = FALSE,
+          borderless = TRUE,
+          filterable = F,
+          theme = reactable::reactableTheme(backgroundColor = "transparent", color = "white"),
+          groupBy = c("Team"),
+          defaultSorted = c("EPVA", "Player", "Team"),
+          
+          columns = list(
+            
+            
+            Team = colDef(html = T,
+                          style = list(fontFamily = "monospace", whiteSpace = "pre", fontWeight = "bold"),
+                          class = "image",
+                          maxWidth = 150,
+                          header = JS("function(colInfo) {
+                                      return colInfo.column.name + '<div style=\"color: #999\">Heat @ Nets (2013)</div>'}")
+                          ),
+            
+            Player = colDef(html = T,
+                            style = list(fontFamily = "monospace", whiteSpace = "pre", fontWeight = "bold"),
+                            align = "center",
+                            class = "imageText",
+                            maxWidth = 200),
+            
+            EPVA = colDef(format = colFormat(digits = 3), 
+                           defaultSortOrder = "desc",
+                           maxWidth = 80,
+                           align = "center",
+                           class = "row1",
+                           style = function(value) {
+                             normalized <- (value - min(epva$EPVA)) / (max(epva$EPVA) - min(epva$EPVA))
+                             color <- orange_pal(normalized)
+                             list(fontFamily = "monospace", whiteSpace = "pre", background = color, color = "black", fontWeight = "bold")}
+            )
+            
+          )
+       )
+                   
